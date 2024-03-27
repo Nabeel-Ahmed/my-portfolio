0945384b6ba183b81998d9e37cb540e6faadef16
Nabeel Ahmed <38479568+Nabeel-Ahmed@users.noreply.github.com>
2024-03-27 05:44:47 +0000

    ci: fixing ci?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
+  output: "export",
   // basePath: "/personal-portfolio",
   images: {
     domains: ["s3-eu-west-1.amazonaws.com"],

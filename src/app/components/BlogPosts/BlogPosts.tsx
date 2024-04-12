2c89e486a6bb67330c157df3e4939129914a0d9e
Nabeel <n.ahmed1307@gmail.com>
2024-04-12 08:44:35 +0100

    feat: :sparkles: added blog section

---
+++ src/app/components/BlogPosts/BlogPosts.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+
+const BlogPosts = () => {
+const [articles, setArticles] = useState<Article[]>([]);
+
+interface Article {
+    id: number;
+    title: string;
+    description: string;
+    url: string;
+    cover_image: string;
+}
+
+  useEffect(() => {
+    fetch("https://dev.to/api/articles?username=mmcode")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.length > 0) {
+          setArticles(data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching blog posts:", error);
+      });
+  }, []);
+
+  return (
+    <div className="pt-4">
+      {articles.map((article) => (
+        <a className="card md:w-2/5  bg-base-300 shadow-xl" href={article.url}>
+            <figure>
+                <img src={article.cover_image} alt="Blog Post Cover"/>
+            </figure>
+          <div className="card-body" key={article.id}>
+            <h2 className="card-title">{article.title}</h2>
+            <p>{article.description}</p>
+          </div>
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default BlogPosts;
